42a9757f31b91559109867916fcbfa5a47c54123
Maciej PIetrzykowski <maciej.pietrzykowski@getindata.com>
2023-10-27 13:28:06 +0200

    fix

--- xxx/dag/dag.py
+++
@@ -1,6 +0,0 @@
-from os import path
-from airflow.models import Variable
-from dbt_airflow_factory.airflow_dag_factory import AirflowDagFactory
-
-
-dag = AirflowDagFactory(path.dirname(path.abspath(__file__)), Variable.get("env")).create()
--- xxx/macros/generate_schema_name.sql
+++
@@ -1,3 +0,0 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
-    {{ dbt_common_macros.custom_generate_schema_name(custom_schema_name, node) }}
-{%- endmacro %}
